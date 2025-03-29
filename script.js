ca84925a36b2c811db214ddf856ced3d1ca068fb
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-29 17:44:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let status = document.getElementById("status");
+let enterBtn = document.getElementById("enterBtn")
+
+function btnClick(){
+	// status.style.backgroundColor = "black";
+	// status.style.color = "white";
+	status.innerHTML = "<h1>Entered Metaverse</h1>"
+}
+enterBtn.addEventListener("click", btnClick)
